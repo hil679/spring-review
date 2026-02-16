4e2df969a3bdb19cca084fe123f3bf2101d2ab99
hil679 <hil679922@gmail.com>
2026-02-17 01:52:26 +0900

    ManyToOne 단방향 test

--- src/main/java/com/example/relation/entity/User.java
+++ src/main/java/com/example/relation/entity/User.java
@@ -13,13 +13,4 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
-
-    // 이 아래부터는 양방향을 위해, 아래 모든 부분 삭제 시 단방향
-    @OneToOne(mappedBy = "user")
-    private Food food;
-
-    public void addFood(Food food) {
-        this.food = food;
-        food.setUser(this);
-    }
 }
