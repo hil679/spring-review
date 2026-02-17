11a7ac052abf0f6bb798da19a1af3da07546ae9c
hil679 <hil679922@gmail.com>
2026-02-17 21:36:18 +0900

    Orphan test

--- src/main/java/com/example/relation/entity/User.java
+++ src/main/java/com/example/relation/entity/User.java
@@ -18,7 +18,12 @@ public class User {
     private String name;
 
     // User Entity를 저장할 때 연관된 Food Entity까지 자동으로 저장
-    @OneToMany(mappedBy = "user", cascade = {CascadeType.PERSIST, CascadeType.REMOVE})
+    @OneToMany(mappedBy = "user", cascade = {CascadeType.PERSIST, CascadeType.REMOVE}
+    , orphanRemoval = true)
+    /* orphanRemoval는 Cascade.REMOVE까지 포함
+        orphanRemoval는 ManyToOne에 없음 -> 해당 객체를 참조하고 있는 다른 객체가 있을 수 있다는 것이기에
+        확인 안 하고 지우면 참조 부결성 위배
+     */
     List<Food> foodList = new ArrayList<>();
 
     public void addFoodList(Food food) {
